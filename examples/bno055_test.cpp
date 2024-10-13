782d4128776d5c4bed68349b555aec4fc8625be2
alpertng02 <alpertunga2003@gmail.com>
2024-10-13 16:08:57 +0300

    added extra configurations to example

--- examples/bno055_test.cpp
+++ examples/bno055_test.cpp
@@ -3,15 +3,32 @@
 #include <thread>
 #include "bno055.hpp"
 
+void sleep_ms(uint32_t ms) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+
 int main() {
 
     imu::BNO055 bno("/dev/i2c-0", 0x28);
+    sleep_ms(10);
 
     if (!bno.isOpen()) {
         std::cout << "Error! Device could not be opened!\n";
         return -1;
     } 
     std::cout << "BNO055 is open!\n";
+
+    if (!bno.setPowerMode(imu::BNO055::PowerMode::Normal)) {
+        std::cout << "Error! BNO055 power mode could not be set!\n";
+        return -1;
+    }
+    sleep_ms(100);
+
+    if (!bno.setOperationMode(imu::BNO055::OperationMode::NDOF)) {
+        std::cout << "Error! BNO055 opearation mode could not be set!\n";
+        return -1;
+    }
+    sleep_ms(100);
 
     while (true) {
         auto [eulerAngles, res] = bno.getEulerAngles();
@@ -20,7 +37,7 @@ int main() {
         } else {
             printf("Error! Could not read from BNO055\n");
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        sleep_ms(10);
     }
     return 0;
 }
